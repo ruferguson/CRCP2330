31267fab00cbb05758ff74ab9eff49bdb0e606c3
ruferguson <ruf@smu.edu>
2021-03-28 14:27:53 -0500

    working through pseudo code for Fill.asm.

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -1,20 +1,23 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
 
-// Runs an infinite loop that listens to the keyboard input.
-// When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen, i.e. writes
-// "white" in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
 
-// pseudocode
-// if (keyPressed) goto BLACK
-// else goto WHITE
+@SCREEN
+D = A
+@addr
+M = D
 
-addr = SCREEN
-n = RAM[0]
-i = 0
+(KEYCHECK)
+
+@KEYPRESSED
+D = M
+
+// go to BLACK if key pressed
+@BLACK
+D; JGT
+
+// go to WHITE if no key pressed
+@WHITE
+D;JEQ
+
+//(continuously loop)
+@KEYCHECK
+0;JMP
