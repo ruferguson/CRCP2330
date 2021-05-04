7290e061da191272af45e6a001d8792323e03f0a
ruferguson <ruf@smu.edu>
2021-05-04 12:49:59 -0500

    added new class Parser and pseudo code from the textbook.

---
+++ 06/Parser.java
@@ -0,0 +1,45 @@
+public class Parser{
+
+	public Parser(File filename) {
+		//opens input file/stream and gets ready to parse it
+	}
+
+	public boolean hasMoreCommands() {
+		// are there more commands in the input
+		return;
+	}
+
+	public void advance() {
+		// reads the next command from 	the input and makes it the current command.
+		// should be called only if hasMoreCommands() is true.
+		// initially there is no current command.
+
+	}
+
+	public CommandType commandType() {
+		// Returns the type of the current command:
+		// - A_COMMAND for @Xxx where Xxx is either a symbol or a decimal number
+		// - C_COMMAND for dest=comp;jump
+		// - L_COMMAND (actually psuedo-command) for (Xxx) where Xxx is a symbol.
+	}
+
+	public String symbol() {
+		// Returns the symbol or decimal Xxx of the current command @Xxx or (Xxx).
+		// Should be called only when commandType() is A_COMMAND or L_COMMAND.
+	}
+
+	public String dest() {
+		// Returns the dest mnemonic of the current C-command (8 possibilities).
+		// Should be called only when commandType() is C_COMMAND.
+	}
+
+	public String comp() {
+		// Returns the comp mnemonic of the current C-command (28 possibilities).
+		// Should be called only when commandType() is C_COMMAND.
+	}
+
+	public String jump() {
+		// Returns the jump mnemonic of the current C-command (8 possibilities).
+		// Should be called only when commandType() is C_COMMAND.
+	}
+}
