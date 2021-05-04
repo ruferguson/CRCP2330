c2b041f6bb1d81491113ce8f64a69c2ba2de3e65
ruferguson <ruf@smu.edu>
2021-05-04 12:36:21 -0500

    added predefined symbols but will now work on an assembler for programs with no symbols as suggested.

--- 06/Assembler.java
+++ 06/Assembler.java
@@ -4,10 +4,12 @@ import java.util.*;
 public class Assembler{
 
 	public static void main(String[] args) {
-		HashMap<String, String> comp = new HashMap<>();
-		HashMap<String, String> dest = new HashMap<>();
-		HashMap<String, String> jump = new HashMap<>();
+		HashMap<String, String> comp = new HashMap<String, String>();
+		HashMap<String, String> dest = new HashMap<String, String>();
+		HashMap<String, String> jump = new HashMap<String, String>();
+		HashMap<String, Integer> symbols = new HashMap<String, Integer>();
 
+		// comp instructions (p.109)
         comp.put("0", "0101010");
 		comp.put("1", "0111111");
 		comp.put("-1", "0111010");
@@ -37,6 +39,7 @@ public class Assembler{
 		comp.put("D|A", "0010101");
 		comp.put("D|M", "1010101");
 
+		// dest instructions (p.110)
         dest.put("NULL", "000");
 		dest.put("M", "001");
 		dest.put("D", "010");
@@ -46,6 +49,7 @@ public class Assembler{
 		dest.put("AD", "110");
 		dest.put("AMD", "111");
 
+		// jump instructions (p.110)
         jump.put("NULL", "000");
 		jump.put("JGT", "001");
 		jump.put("JEQ", "010");
@@ -54,6 +58,32 @@ public class Assembler{
 		jump.put("JNE", "101");
 		jump.put("JLE", "110");
 		jump.put("JMP", "111");
+
+		// predefined symbols (p. 110)
+		symbols.put("SP", 0);
+		symbols.put("LCL", 1);
+		symbols.put("ARG",2);
+		symbols.put("THIS",3);
+        symbols.put("THAT",4);
+        symbols.put("R0",0);
+        symbols.put("R1",1);
+        symbols.put("R2",2);
+        symbols.put("R3",3);
+        symbols.put("R4",4);
+        symbols.put("R5",5);
+        symbols.put("R6",6);
+        symbols.put("R7",7);
+        symbols.put("R8",8);
+        symbols.put("R9",9);
+        symbols.put("R10",10);
+        symbols.put("R11",11);
+        symbols.put("R12",12);
+        symbols.put("R13",13);
+        symbols.put("R14",14);
+        symbols.put("R15",15);
+        symbols.put("SCREEN",16384);
+        symbols.put("KBD",24576);
+
 	}
 
 }
